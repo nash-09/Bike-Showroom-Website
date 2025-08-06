9b16642ec3e27c0c4b50f4d54746135031676c28
Nash-09 <niranjanbarupal182@gmail.com>
2025-08-06 14:42:29 +0530

    Initial Commit

--- home/home.js
+++ home/home.js
@@ -6,49 +6,49 @@ btn.onclick = () => {
     let value = data.value.toLowerCase();
 
     if(value === "ducati" ){
-        window.location.href="../ducati/ducati.html"
+        window.location.href="ducati/ducati.html"
     }
     else if(value === "ducati panigale v4 r"){
-        window.location.href="../ducati/panigale/panigale.html"
+        window.location.href="ducati/panigale/panigale.html"
     }
     else if(value === "ducati monster"){
-        window.location.href="../ducati/monster/monster.html"
+        window.location.href="ducati/monster/monster.html"
     }
     else if(value === "kawasaki"){
-        window.location.href="../kawasaki/kawasaki.html"
+        window.location.href="kawasaki/kawasaki.html"
     }
     else if(value === "kawasaki ninja h2 sx"){
-        window.location.href="../kawasaki/ninja h2 sx/ninja h2 sx.html"
+        window.location.href="kawasaki/ninja h2 sx/ninja h2 sx.html"
     }
     else if(value === "kawasaki z900"){
-        window.location.href="../kawasaki/z 900/z 900.html"
+        window.location.href="kawasaki/z 900/z 900.html"
     }
     else if(value === "bmw"){
-        window.location.href="../bmw/bmw.html"
+        window.location.href="bmw/bmw.html"
     }
     else if(value === "bmw m 1000 rr"){
-        window.location.href="../bmw/m 1000 rr/m 1000 rr.html"
+        window.location.href="bmw/m 1000 rr/m 1000 rr.html"
     }
     else if(value === "bmw g 310 rr"){
-        window.location.href="../bmw/g310rr/g310rr.html"
+        window.location.href="bmw/g310rr/g310rr.html"
     }
     else if(value === "triumph"){
-        window.location.href="../triumph/triumph.html"
+        window.location.href="triumph/triumph.html"
     }
     else if(value === "triumph speed triple 1200 rs"){
-        window.location.href="../triumph/speed triple 1200 rs/speed triple 1200 rs.html"
+        window.location.href="triumph/speed triple 1200 rs/speed triple 1200 rs.html"
     }
     else if(value === "triumph rocket 3 r"){
-        window.location.href="../triumph/rocket 3 r/rocket 3 r.html"
+        window.location.href="triumph/rocket 3 r/rocket 3 r.html"
     }
     else if(value === "aprilia"){
-        window.location.href="../aprilia/aprilia.html"
+        window.location.href="aprilia/aprilia.html"
     }
     else if(value === "aprilia rsv4 factory 1100"){
-        window.location.href="../aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
+        window.location.href="aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
     }
     else if(value === "aprilia tuono 457"){
-        window.location.href="../aprilia/tuono 457/tuono457.html"
+        window.location.href="aprilia/tuono 457/tuono457.html"
     }
     else{
         alert("Please enter the valid bike name!")
@@ -69,23 +69,23 @@ let triumph = document.querySelector(".triumph")
 let aprilia = document.querySelector(".aprilia")
 
 ducati.onclick = () => {
-    window.location.href="../ducati/ducati.html"
+    window.location.href="ducati/ducati.html"
 }
 
 kawasaki.onclick = () => {
-    window.location.href="../kawasaki/kawasaki.html"
+    window.location.href="kawasaki/kawasaki.html"
 }
 
 bmw.onclick = () => {
-    window.location.href="../bmw/bmw.html"
+    window.location.href="bmw/bmw.html"
 }
 
 triumph.onclick = () => {
-    window.location.href="../triumph/triumph.html"
+    window.location.href="triumph/triumph.html"
 }
 
 aprilia.onclick = () => {
-    window.location.href="../aprilia/aprilia.html"
+    window.location.href="aprilia/aprilia.html"
 }
 
 
@@ -103,43 +103,43 @@ let triumphTopModel1 = document.querySelector("#triumph-model")
 let apriliaTopModel1 = document.querySelector("#aprilia-model")
 
 ducatiTopModel.onclick = () => {
-    window.location.href="../ducati/panigale/panigale.html"
+    window.location.href="ducati/panigale/panigale.html"
 }
 
 kawasakiTopModel.onclick = () => {
-    window.location.href="../kawasaki/ninja h2 sx/ninja h2 sx.html"
+    window.location.href="kawasaki/ninja h2 sx/ninja h2 sx.html"
 }
 
 bmwTopModel.onclick = () => {
-    window.location.href="../bmw/m 1000 rr/m 1000 rr.html"
+    window.location.href="bmw/m 1000 rr/m 1000 rr.html"
 }
 
 triumphTopModel.onclick = () => {
-    window.location.href="../triumph/speed triple 1200 rs/speed triple 1200 rs.html"
+    window.location.href="triumph/speed triple 1200 rs/speed triple 1200 rs.html"
 }
 
 apriliaTopModel.onclick = () => {
-    window.location.href="../aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
+    window.location.href="aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
 }
 
 ducatiTopModel1.onclick = () => {
-    window.location.href="../ducati/panigale/panigale.html"
+    window.location.href="ducati/panigale/panigale.html"
 }
 
 kawasakiTopModel1.onclick = () => {
-    window.location.href="../kawasaki/ninja h2 sx/ninja h2 sx.html"
+    window.location.href="kawasaki/ninja h2 sx/ninja h2 sx.html"
 }
 
 bmwTopModel1.onclick = () => {
-    window.location.href="../bmw/m 1000 rr/m 1000 rr.html"
+    window.location.href="bmw/m 1000 rr/m 1000 rr.html"
 }
 
 triumphTopModel1.onclick = () => {
-    window.location.href="../triumph/speed triple 1200 rs/speed triple 1200 rs.html"
+    window.location.href="triumph/speed triple 1200 rs/speed triple 1200 rs.html"
 }
 
 apriliaTopModel1.onclick = () => {
-    window.location.href="../aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
+    window.location.href="aprilia/rsv4 factory 1100/rsv4 factory 1100.html"
 }
 
 // sell-section
@@ -147,7 +147,7 @@ apriliaTopModel1.onclick = () => {
 let button = document.querySelector("#sell-section-btn")
 
 button.onclick = () => {
-    window.location.href="../sell bikes/sell bikes.html"
+    window.location.href="sell bikes/sell bikes.html"
 }
 
 
